04c4c2c21b7686a96d9e92debdd8826701b1def4
Freddie <freddie.smith@sentium-consulting.com>
2023-06-03 14:02:50 +0100

    convert to ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-function hasPermission(user, permissionsNeeded) {
+const hasPermission = (user: { permissions: [any] }, permissionsNeeded: Array<any>) => {
   const matchedPermissions = user.permissions.filter(permissionTheyHave =>
     permissionsNeeded.includes(permissionTheyHave)
   );
